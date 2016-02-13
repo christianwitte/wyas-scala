f63429fc984ac3c2a4cf0cee5de0b573b594c65b
Christian Witte <christian.witte@gmail.com>
2016-02-13 14:39:20 -0500

    Added parser for String and Atom

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -3,17 +3,33 @@ package parser
 import atto._
 import Atto._
 import scalaz._
-import Scalaz._
+//import Scalaz._
 
 object Parser {
-  def symbol = oneOf("!#$%&|*+-/:<=>?@^_~")
+    def symbol = oneOf("!#$%&|*+-/:<=>?@^_~")
 
-  def readExpr(expr: String): String = {
-    symbol.parse(expr).either match {
-      case -\/(l) => "No match: " ++ l
-      case \/-(v) => "Found: " ++ v.toString
+    def parseString: Parser[LispVal] = for {
+        _ ← char('"')
+        x ← many(noneOf("\\"))
+        _ ← char('"')
+    } yield (StringLV(x.mkString))
+
+    def parseAtom: Parser[LispVal] = for {
+        first ← letter | symbol
+        rest ← many(letter | digit | symbol)
+        atom = first +: rest
+    } yield atom.mkString match {
+        case "#t" ⇒ BoolLV(true)
+        case "#f" ⇒ BoolLV(false)
+        case a ⇒ Atom(a)
     }
-  }
+
+    def readExpr(expr: String): String = {
+        symbol.parse(expr).either match {
+            case -\/(l) ⇒ "No match: " ++ l
+            case \/-(v) ⇒ "Found: " ++ v.toString
+        }
+    }
 
 }
 
